ee68854a1b9f1b8bd2940be0682ce7c5188c2371
ZHENK <chengzhenyang@gmail.com>
2021-08-01 15:14:09 +1200

    Add SOCKET.IO

--- pi/main.py
+++ pi/main.py
@@ -1,22 +1,26 @@
 from gpiozero import Buzzer, OutputDevice
 import websockets, asyncio
+from time import sleep
+import socketio
 
 BUZZER_PIN = 5
 RELAY_PIN= 3
+
+UID="6105e7c6396ab2ee56cf3c67"
+SOCKETIO="wss://bitgud-backend.herokuapp.com"
 
 buzzer = Buzzer(BUZZER_PIN)
 relay = OutputDevice(RELAY_PIN, active_high=True, initial_value=False)
 
 async def listen():
-    url = ''
+    await sio.connect(SOCKETIO,auth={token: UID})
 
-    async with websockets.connect(url) as ws:
-        while True:
-            msg = await ws.recv()
-            if (msg == 'on'):
-                buzzer.on()
-                relay.on()
-                buzzer.off()
-                relay.off()
+    @sio.on('shock')
+    def shock():
+        # handle the message
+        relay.on()
+        sleep(1)
+        relay.off()
+            
 
 asyncio.get_event_loop().run_forever(listen())
